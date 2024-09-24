9f46be4b24cccaeae72420dc4cfd62da166080e7
pabloHuerres01 <pablomartinezhuerres2001@gmail.com>
2024-09-24 22:14:24 +0200

    main

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,8 @@ public class Main {
 	public Main()
 	{
 		Cliente cliente = new Cliente();
+		cliente.nuevocliente();
+		//cliente.escribirEnArchivo(cliente);
 	}
 
 	public static void main(String[] args) {
